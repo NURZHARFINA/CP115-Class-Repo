aca317a111b148e4faba5d6288e93b30f1d13e7a
Willim2208 <m-9385950@moe-dl.edu.my>
2026-07-15 12:31:27 +0800

    lab04 exercise1

--- labs/lab04/exercise1/lab-4-1.py
+++ labs/lab04/exercise1/lab-4-1.py
@@ -5,5 +5,5 @@ else:
     if kWh >100 and kWh <= 200 :
         totalBill = (100*0.3) + ((kWh - 100) *0.5)
     else: 
-        totalBill = (100 * 0.3) + (200 * 0.5) + ((kWh - 100 - 200)*0.75)
+        totalBill = (100 * 0.3) + (100 * 0.5) + ((kWh - 100 - 100)*0.75)
 print(totalBill)
